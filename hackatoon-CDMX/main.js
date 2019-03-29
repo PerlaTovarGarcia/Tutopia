7d064d31e2234764f621b7a2c42b80654e9d7569
Perla <pataditaenloshuevos@gmail.com>
2019-03-29 08:20:59 -0600

    imagenes acomodadas

--- hackatoon-CDMX/main.js
+++ hackatoon-CDMX/main.js
@@ -3,16 +3,30 @@
 //Establecer variables para indicar la ruta a la carpeta incluye imágenes.
    let list = [];
 //indica la ruta completa a las imágenes, incluido su nombre de archivo.
+  let body= "./cuerpos/";
+  let listBody= [];
+
+  let feet = "./pies/";
+  let listFeet = [];
 
 
-
-          for(let i=0 ; i < 10; i++) {
+          for(let i=0 ; i < 8; i++) {
               list[i] = directry + i + ".png";
 //let list = "directorio a carpeta" + "uno de los números (i)" + "extensión (png)" .
             new  Image().src = list[i];
 //Crear imagen de la cual src es la lista [i].
   }
 
+          for(let j=0; j < 8; j++) {
+            listBody[j] = body + j + ".png";
+            new Image().src=listBody[j];
+          }
+
+
+          for(let p=0; p < 8; p++) {
+            listFeet[p] = feet + p + ".png";
+            new Image().src=listFeet[p];
+          }
 
 //variale para hacer un contador.
 let counter = 0;
@@ -22,20 +36,20 @@ let counter = 0;
 // Comienza a generar números al azar. En este caso, el proceso de generación se muestra antes de que se haya decidido el número.
               counter++;
 //Contador para contar el proceso de generación de tiempos.
-              let left = Math.floor(Math.random() * 10);
-              let center = Math.floor(Math.random() * 10);
-              let right = Math.floor(Math.random() * 10);
+              let up = Math.floor(Math.random() * 8);
+              let center = Math.floor(Math.random() * 8);
+              let down = Math.floor(Math.random() * 8);
  // El número se genera en punto decimal, de modo que el número debe multiplicarse por 10. Luego, el número se redondeará a entero por "piso".
-              document.left.src = list[left];
-              document.center.src = list[center];
-              document.right.src = list[right];
+              document.up.src = list[up];
+              document.center.src = listBody[center];
+              document.down.src = listFeet[down];
 //Mostrar los números durante el proceso de generación de números.
 
 
               if (counter > 5){
-                 let final_left = list[left];
-                 let final_center = list[center];
-                 let final_right = list[right];
+                 let final_up = list[up];
+                 let final_center = listBody[center];
+                 let final_down = listFeet[down];
 // Mostrar cinco números durante el proceso de generación en 10 mm / seg.
 
 
